d15c59f10568bf81029af740253fe6c168c05c26
riet <adamriet@gmail.com>
2023-03-15 17:26:52 -0600

    added function for timer

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -4,51 +4,88 @@ var questions = [
     {
       question: "Which of the following is used to define a function in JavaScript?",
       answers: {
-        a: 'def',
-        b: 'function',
-        c: 'var',
-        d: 'let'
+        a: "def",
+        b: "function",
+        c: "var",
+        d: "let"
       },
-      correctAnswer: 'b'
+      correctAnswer: "b"
     },
     {
         question: "Which of the following is not a JavaScript data type?",
         answers: {
-          a: 'Number',
-          b: 'String',
-          c: 'Boolean',
-          d: 'Character'
+          a: "Number",
+          b: "String",
+          c: "Boolean",
+          d: "Character"
         },
-        correctAnswer: 'd'
+        correctAnswer: "d"
       },
       {
         question: "Which of the following is used to select an HTML element by its id in JavaScript?",
         answers: {
-          a: 'querySelector',
-          b: 'getElementById',
-          c: 'getElementsByClassName',
-          d: 'getElementsByTagName'
+          a: "querySelector",
+          b: "getElementById",
+          c: "getElementsByClassName",
+          d: "getElementsByTagName"
         },
-        correctAnswer: 'b'
+        correctAnswer: "b"
       },
       {
         question: "Which of the following is used to loop through the elements of an array in JavaScript?",
         answers: {
-          a: 'while loop',
-          b: 'for loop',
-          c: 'do-while loop',
-          d: 'switch-statement'
+          a: "while loop",
+          b: "for loop",
+          c: "do-while loop",
+          d: "switch-statement"
         },
-        correctAnswer: 'b'
+        correctAnswer: "b"
       },
       {
         question: "What is the purpose of the this keyword in JavaScript?",
         answers: {
-          a: 'To refer to the current function',
-          b: 'To refer to the global object',
-          c: 'To refer to the parent object',
-          d: 'To refer to the current object'
+          a: "To refer to the current function",
+          b: "To refer to the global object",
+          c: "To refer to the parent object",
+          d: "To refer to the current object"
         },
-        correctAnswer: 'd'
+        correctAnswer: "d"
       }
   ];
+
+  //Variables 
+
+var quizSection = document.querySelector("quizSection");
+var startQuiz = document.querySelector("startQuiz");
+var viewHighScore = document.querySelector("viewHighScore");
+var timeLeft = document.querySelector("#timeLeft");
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //Event Listener to start Timer
+  
+  var secondsLeft = 60;
+
+  function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeLeft.textContent = secondsLeft + " seconds left.";
+  
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+       }
+  
+    }, 1000);
+  }
+  
+  setTime();
