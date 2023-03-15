8a5b9ca4fc523cbe5ed98575774fa8e232f0a4de
riet <adamriet@gmail.com>
2023-03-15 15:30:23 -0600

    added questions to java script

---
+++ assets/js/scipt.js
@@ -0,0 +1,54 @@
+// Code containing questions
+
+var questions = [
+    {
+      question: "Which of the following is used to define a function in JavaScript?",
+      answers: {
+        a: 'def',
+        b: 'function',
+        c: 'var',
+        d: 'let'
+      },
+      correctAnswer: 'b'
+    },
+    {
+        question: "Which of the following is not a JavaScript data type?",
+        answers: {
+          a: 'Number',
+          b: 'String',
+          c: 'Boolean',
+          d: 'Character'
+        },
+        correctAnswer: 'd'
+      },
+      {
+        question: "Which of the following is used to select an HTML element by its id in JavaScript?",
+        answers: {
+          a: 'querySelector',
+          b: 'getElementById',
+          c: 'getElementsByClassName',
+          d: 'getElementsByTagName'
+        },
+        correctAnswer: 'b'
+      },
+      {
+        question: "Which of the following is used to loop through the elements of an array in JavaScript?",
+        answers: {
+          a: 'while loop',
+          b: 'for loop',
+          c: 'do-while loop',
+          d: 'switch-statement'
+        },
+        correctAnswer: 'b'
+      },
+      {
+        question: "What is the purpose of the this keyword in JavaScript?",
+        answers: {
+          a: 'To refer to the current function',
+          b: 'To refer to the global object',
+          c: 'To refer to the parent object',
+          d: 'To refer to the current object'
+        },
+        correctAnswer: 'd'
+      }
+  ];
